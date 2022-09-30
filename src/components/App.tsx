622996f4fb77a29c6e8b752bd8f4fbd1047d8ba9
lebedev-023046 <lebedev.023046@gmail.com>
2022-09-30 17:55:17 +0300

    feat: add initial configuration

--- src/components/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const App = () => {
-    return (
-        <h1 className="text-black/50 text-3xl font-bold text-center py-4">Hello react</h1>
-    )
-  } 
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,78 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin,
+} = require('clean-webpack-plugin')
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.jsx'),
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
             loader: 'babel-loader',
           },
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: 'babel-loader',
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true,
             },
-            {
-              loader: 'react-svg-loader',
-              options: {
-                jsx: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: ['file-loader'],
-        },
-        {
-          test: /\.(jpg|png)$/,
-          use: {
-            loader: 'url-loader',
           },
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
+        ],
       },
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: path.resolve(
-          __dirname,
-          './public/index.html',
-        ),
-        filename: 'index.html',
-      }),
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@assets': path.resolve('./src/assets'),
+      '@components': path.resolve('./src/components'),
+      '@constants': path.resolve('./src/constants'),
+      '@pages': path.resolve('./src/pages'),
+      '@store': path.resolve('./src/store'),
+      '@utils': path.resolve('./src/utils'),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        './public/index.html',
+      ),
+      filename: 'index.html',
+    }),
+  ],
 }
 
 module.exports = ({ mode }) => {
