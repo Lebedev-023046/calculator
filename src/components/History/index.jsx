db81dba633ee89fac9cb618547575b46b0eed496
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-06 19:13:05 +0300

    feat: add History component

---
+++ src/components/History/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styled'
+
+export default function History() {
+    return (
+        <S.SHistoryContainer>
+          <S.SHistoryTitle>
+            History
+          </S.SHistoryTitle>
+        </S.SHistoryContainer>
+    )
+}
---
+++ src/components/History/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const SHistoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  width: 8rem;
+  height: 100%;
+  border-left: 3px solid black;
+`
+
+export const SHistoryTitle = styled.div`
+  width: 100%;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+`
