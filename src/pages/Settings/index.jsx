3c2e8d3bb868d9b45842b3fdb877b80cbf753a71
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-06 19:16:36 +0300

    feat: add Settings component

---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as S from './styled'
+
+export function Settings() {
+    return (
+        <S.SSettingsTitle>Settings Page</S.SSettingsTitle>
+    )
+}
---
+++ src/pages/Settings/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const SSettingsTitle = styled.div`
+  text-align: center;
+  font-size: 32px;
+  padding: 20px 0;
+`
