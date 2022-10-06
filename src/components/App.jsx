d5443d0a649ec19986ed58d043af4f411e678b31
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-06 19:02:08 +0300

    feat: add routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import GlobalStyle from '@/globalStyles'
 import React from 'react'
-import Header from './Header/index'
+import Header from '@components/Header'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from '@/pages/home'
+import { Settings } from '@/pages/settings'
 
 export const App = () => {
     return (
       <React.Fragment>
         <GlobalStyle/>
         <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </React.Fragment>
     )
   }
