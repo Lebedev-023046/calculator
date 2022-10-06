3671aa2af578046a9d57daddcce5174209009455
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-06 19:26:31 +0300

    feat: add  initial appReducer

---
+++ src/redux/reducers.js
@@ -0,0 +1,27 @@
+import { ADD_VALUE } from './actions'
+import { combineReducers } from 'redux'
+
+const initialState = {
+  expressionHistory: '',
+  currentValue: '',
+  historyList: [],
+}
+
+export const appReducer = (
+  state = initialState,
+  { type, payload },
+) => {
+  switch (type) {
+    case ADD_VALUE:
+      return {
+        ...state,
+        expression: state.expression + payload.value,
+      }
+    default:
+      return state
+  }
+}
+
+export const rootReducer = combineReducers({
+  appReducer: appReducer,
+})
