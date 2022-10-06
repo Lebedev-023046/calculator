7d22ca03ed9dc4e2b9da4b4022be2d3672686e40
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-06 19:01:56 +0300

    feat: add App wrappers

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from "react"
 import reactDom from "react-dom/client"
 import { App } from "@components/App"
+import { Provider } from "react-redux"
+import { BrowserRouter } from 'react-router-dom'
+import store from "@redux/store"
 
 
 const root = reactDom.createRoot(document.getElementById("root"))
 
 root.render(
-  <App/>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
 )
