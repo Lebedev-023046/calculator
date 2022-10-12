98b3be678938ef3e48bf7f92aed07875b44bccf0
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-12 20:38:04 +0300

    feat: add initial processExpressoin function

---
+++ src/helpers/index.js
@@ -0,0 +1,14 @@
+const processExpression = (event, dispatch) => {
+  switch (event.target.textContent) {
+    case '=':
+      dispatch()
+      break
+    case 'CE':
+      break
+    case 'C':
+      break
+
+    default:
+      break
+  }
+}
