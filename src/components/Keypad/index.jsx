c5ca6b7c84d6fef8191bc85a8b7d9b0040549dc4
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-12 20:41:31 +0300

    feat: add habdleKeypad function

--- src/components/Keypad/index.jsx
+++ src/components/Keypad/index.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import * as S from './styled'
 import { KeypadButton } from '../KeypadButton'
 import { btnVals } from '@/constants/btnVals'
+import { useDispatch } from 'react-redux'
+import { addValue } from '@/redux/actions'
+
+
 
 export default function Keypad() {
+  const dispatch = useDispatch()
+
+  const processVals = ['=', 'CE', 'C']
+  const handleKeypad = event => {
+    if (event.target.parentNode.classList.contains('keypadContainer'))
+      if (processVals.includes(event.target.textContent))  {
+        console.log('function for processing expression')
+      }else {
+        dispatch(addValue(event.target.textContent))
+      }
+    }
+
     return (
-      <S.SKeypadContainer>
+      <S.SKeypadContainer className="keypadContainer" onClick={event => handleKeypad(event)}>
         { btnVals.map(btnValue =>
           <KeypadButton key={btnValue.id} value={btnValue.key}/>,
         )}
