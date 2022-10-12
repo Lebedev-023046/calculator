336465c60077928346cdb6aacabe478c5dccce52
lebedev-023046 <lebedev.023046@gmail.com>
2022-10-12 20:42:14 +0300

    refactor: change var

--- src/components/Display/index.jsx
+++ src/components/Display/index.jsx
@@ -4,14 +4,14 @@ import { useSelector } from 'react-redux'
 import * as S from './styled'
 
 export default function Display() {
-  const { currentValue  } = useSelector(state => state.appReducer)
+  const { expression  } = useSelector(state => state.appReducer)
 
   return (
     <div>
       <S.SDisplayContainer>
         <S.SKeyboardContainer>
           <S.SDisplayInputField>
-            { currentValue }
+            { expression }
           </S.SDisplayInputField>
         </S.SKeyboardContainer>
       </S.SDisplayContainer>
